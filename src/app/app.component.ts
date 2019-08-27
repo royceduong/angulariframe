5154cd09d1af71363e3a6c8895a48b3d3a046cea
Justin Tran <justint@phenomenex.com>
2019-08-27 10:17:28 -0700

    Update with legacy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform, OnInit } from '@angular/core';
 import { EventListener } from '@angular/core/src/debug/debug_node';
 import { EventManager } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sampleApp';
   results: any;
+  link: string;
+
+  ngOnInit() {
+    this.link = "http://localhost:49511/Tools/GCLinerTool?tab=Geometry";
+    //this.link = "http://localhost:4200/assets/iframepage.html";
+  }
 
   //Callback function to process data from window.
   recieveMessage = (event: MessageEvent) => {
-    if( typeof event.data === "number"){
+    if (typeof event.data === "string") {
       this.results = event.data
     }
-    else if(event.data.data == undefined){
+    else if (event.data.data == undefined) {
       this.results = undefined;
     }
   }
 
-  constructor(){
+  constructor() {
     //Event Listener for postMessage() from iframe.
-    if(window.addEventListener){
+    if (window.addEventListener) {
       window.addEventListener("message", this.recieveMessage.bind(this), false)
     }
   }
 
   //Send postMessage() to iframe
-  myFunction(){
+  myFunction() {
     var message = Math.random()
     var iframe = document.getElementById("iframeComponent")
     var iWindow = (<HTMLIFrameElement>iframe).contentWindow;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, SafePipe } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
